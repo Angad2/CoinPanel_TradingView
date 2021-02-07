520dc90c5e8bb08b9cc4d2841988c5eaf528696c
Angad <angad.kumar@cbnits.com>
2021-02-07 19:23:17 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, ToastAndroid } from 'react-native';
+import { AppRegistry} from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class app extends Component {
@@ -7,23 +7,14 @@ export default class app extends Component {
   render() {
     return (
       <WebView
-        style={{ flex: 1, width: '100%', height: '100%' }}
+        style={{ flex: 1}}
         originWhitelist={["*"]}
-        //source={{ uri: 'https://tv-tut-part2.glitch.me/' }}
-        source={{ uri: 'file:///android_asset/index.html' }}
+        source={{ uri: 'https://coinbase-assignment.herokuapp.com/' }}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         allowFileAccessFromFileURLs={true}
         allowFileAccess={true}
         allowUniversalAccessFromFileURLs={true}
-        // onShouldStartLoadWithRequest={() => false}
-        injectedJavaScript={this.jsToInject}
-        onMessage={(event) => {
-          const data = JSON.parse(event.nativeEvent.data)
-          if (data.type == "onIntervalChanged") {
-            ToastAndroid.show("Interval = " + data.interval, ToastAndroid.SHORT);
-          }
-        }}
       />
     );
   }
